107aa861e38c7505795c2073d10f1d4eb44ca03a
Modestas <modestask97@gmail.com>
2018-05-15 19:41:47 +0100

    newsletter component

--- src/components/NewsLetter.js
+++ src/components/NewsLetter.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
 
 class NewsLetter extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [
+        {
+          name: 'src/assets/children-s-copy-2.png',
+          image: 'src/assets/bitmap-copy-2.png'
+        },
+        {
+          name: 'src/assets/crafts-hobbies-copy.png',
+          image: 'src/assets/bitmap-copy-9.png'
+        }
+      ]
+    };
+  }
+
   render() {
     return (
       <div className="blue-bg">
@@ -8,6 +25,23 @@ class NewsLetter extends Component {
           className="Book-Recommendations"
           src={require('../assets/book-recommendations.png')}
         />
+        <img
+          className="categories"
+          src={require('../assets/choose-from-the-cate.png')}
+        />
+        <div>
+          {
+            this.state.categories.map(( category, i )=> {
+              return(
+                <div className="category-block" key={i}>
+                  <input type="checkbox" />
+                  <img className="category-name" src={require(category.name)}/>
+                  <img className="category-image" src={require(category.image)}/>
+                </div>
+              );
+            })
+          }
+        </div>
       </div>
     );
   }
