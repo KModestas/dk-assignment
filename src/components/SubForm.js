c84e1c18eadb1c930d92914dfa66feb8c5342505
Modestas <modestask97@gmail.com>
2018-05-16 15:09:43 +0100

    subform submit logic

--- src/components/SubForm.js
+++ src/components/SubForm.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 
-const SubForm = ({})=> {
+class SubForm extends Component {
+
+  state = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    terms: ''
+  }
+
+  change = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault()
+    console.log(this.state);
+  }
+
+  render() {
     return(
       <form className="subform-container">
         <h1>SUB FORM</h1>
@@ -9,30 +29,31 @@ const SubForm = ({})=> {
           type="text"
           placeholder="First Name*"
           name="firstName"
-          onChange={handleChange}
-          value={user.firstName}
+          onChange={this.change}
+          value={this.state.firstName}
           />
         <input
           type="text"
           placeholder="Last Name *"
           name="lastName"
-          onChange={handleChange}
-          value={user.lastName}
+          onChange={this.change}
+          value={this.state.lastName}
         />
         <input
           type="text"
           placeholder="Your email address *"
           name="email"
-          onChange={handleChange}
-          value={user.email}
+          onChange={this.change}
+          value={this.state.email}
         />
-        <button>Submit</button>
+        <button onClick={e => this.onSubmit(e)}>Submit</button>
         <input
           type="checkbox"
         />
       </form>
     );
   }
+}
 
 
 
