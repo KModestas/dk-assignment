3ebb7e108b28334b966be3eb8fae258a34e5146c
Modestas <modestask97@gmail.com>
2018-05-16 16:10:48 +0100

    form validation continued

--- src/components/SubForm.js
+++ src/components/SubForm.js
@@ -5,20 +5,72 @@ class SubForm extends Component {
 
   state = {
     firstName: '',
+    firstNameError: '',
+
     lastName: '',
+    lastNameError: '',
+
     email: '',
-    terms: ''
+    emailError: '',
+
+    terms: '',
+    termsError: ''
   }
 
   change = e => {
     this.setState({
       [e.target.name]: e.target.value
     })
+
+    console.log(this.state)
+  }
+
+  validate = () => {
+    let isError = false;
+    const errors = {}
+
+    if(!this.state.firstName) {
+      isError = true;
+      errors.firstNameError = 'Please enter your first name'
+    }
+
+    if(!this.state.lastName) {
+      isError = true;
+      errors.lastNameError = 'Please enter your last name'
+    }
+
+    if(!this.state.email) {
+      isError = true;
+      errors.emailError = 'Please enter your email address'
+    }
+
+    // if(!this.state.firstName) {
+    //   isError = true;
+    //   errors.firstNameError = 'Please enter your first name'
+    // }
+
+    if (isError) {
+      this.setState({
+        ...errors
+      });
+    }
+
+    return isError;
   }
 
   onSubmit = (e) => {
     e.preventDefault()
     console.log(this.state);
+
+    const err = this.validate();
+      if (!err) {
+      this.setState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        terms: ''
+      })
+    }
   }
 
   render() {
@@ -31,6 +83,7 @@ class SubForm extends Component {
           name="firstName"
           onChange={this.change}
           value={this.state.firstName}
+          errortext={this.state.firstNameError}
           />
         <input
           type="text"
@@ -38,6 +91,7 @@ class SubForm extends Component {
           name="lastName"
           onChange={this.change}
           value={this.state.lastName}
+          errortext={this.state.lastNameError}
         />
         <input
           type="text"
@@ -45,6 +99,7 @@ class SubForm extends Component {
           name="email"
           onChange={this.change}
           value={this.state.email}
+          errortext={this.state.emailError}
         />
         <button onClick={e => this.onSubmit(e)}>Submit</button>
         <input
