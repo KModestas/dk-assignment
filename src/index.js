c93d86211a8e92613b7fa7a2dc31afa67b9e2cc5
Modestas <modestask97@gmail.com>
2018-05-21 12:56:45 +0100

    added and connected store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { Provider } from 'react-redux';
+import store from './store';
 
 ReactDOM.render(
-  <App />
-  , document.getElementById('root'));
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+
+
+
+ // provider function wraps around entire react app which allows react to gain access to redux. The store is passed as a prop to the redux provider component.
